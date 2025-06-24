8316286bc8d7055c998637d9f739b9d4b889dcca
Satwik Baramal <satwikbaramal854.gmail.com>
2025-06-24 05:10:19 +0000

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Container, Button, Card, Spinner } from "react-bootstrap";
 import './App.css';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
